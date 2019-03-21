fe772798737f7af440fa2504f6478483f732ced5
aorra <aorra@users.noreply.github.com>
2019-03-21 12:40:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");    
     //res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    res.header("X-Content-Type-Options", "nosniff");
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
